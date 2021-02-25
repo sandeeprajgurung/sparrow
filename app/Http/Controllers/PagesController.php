e6526cd4399d63c872c680cdfc189a1c731f530e
sandeeprajgurung <smile2srg@gmail.com>
2021-02-25 21:32:14 +0545

    design setup

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+
+    public function home() {
+        return view('frontend.pages.home');
+    }
+}
